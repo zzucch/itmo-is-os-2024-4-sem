8275477ee0c665c66b9ebb00cfbd5fb0bf7504b4
pc <nikitamilovatskiy@gmail.com>
2024-02-21 04:22:41 +0300

    fix: make closer to the task

--- lab1/1-6.sh
+++ lab1/1-6.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+rm -f full.log
 touch full.log
 
 prev=false
 
 awk '{
 if ($3 == "(WW)") {
-  $3 = "Warning"
+  $3 = "Warning:"
   prev = true
 
   print($0)
@@ -15,11 +16,11 @@ if ($3 == "(WW)") {
 } else {
   prev = false
 }
-}' /home/pc/Downloads/anaconda/syslog >full.log
+}' /home/pc/Downloads/anaconda/X.log >full.log
 
 awk '{
 if ($3 == "(II)") {
-  $3 = "Information"
+  $3 = "Information:"
   prev = true
   print($0)
 } else if (substr($3,0,1) != "(" && substr($3,3,1) != ")" && prev == true){
@@ -27,6 +28,6 @@ if ($3 == "(II)") {
 } else {
   prev=false
 }
-}' /home/pc/Downloads/anaconda/syslog >full.log
+}' /home/pc/Downloads/anaconda/X.log >full.log
 
 cat full.log
