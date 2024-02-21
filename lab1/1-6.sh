d4a25de886f229efb7e841df72e3a0bc3b41d33f
pc <nikitamilovatskiy@gmail.com>
2024-02-21 12:56:43 +0300

    fix: make 1-6 work as expected

--- lab1/1-6.sh
+++ lab1/1-6.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-rm -f full.log
-touch full.log
-
-prev=false
-
 awk '{
 if ($3 == "(WW)") {
   $3 = "Warning:"
-  prev = true
-
   print($0)
-} else if (substr($3, 0, 1) != "(" && substr($3, 3, 1) && prev == true) {
-  print($0)
-} else {
-  prev = false
 }
 }' /home/pc/Downloads/anaconda/X.log >full.log
 
 awk '{
+prev = false
 if ($3 == "(II)") {
   $3 = "Information:"
   prev = true
@@ -28,6 +18,6 @@ if ($3 == "(II)") {
 } else {
   prev=false
 }
-}' /home/pc/Downloads/anaconda/X.log >full.log
+}' /home/pc/Downloads/anaconda/X.log >>full.log
 
 cat full.log
