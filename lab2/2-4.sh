6df94279ffe0572f0d2e7a56213ab2ee2b696c78
zzucch <nikitamilovatskiy@gmail.com>
2024-03-16 19:30:55 +0300

    feat: add 2-4

--- lab2/2-4.sh
+++ lab2/2-4.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-for pid in /proc/[0-9]+; do
-  [ -d "$pid" ] || continue
+for pid in $(ls /proc | grep -E '^[0-9]+$'); do
+	if [[ ! -d "/proc/$pid" ]]; then
+		continue
+	fi
 
-  ppid=$(awk -F: '/PPid/ {print $2}' "$pid/status"
+	ppid=$(awk -F: '/PPid/ {print $2}' "/proc/$pid/status" | tr -d '\t')
 
-  sum_exec_runtime=$(awk -F: '/se.sum_exec_runtime/ {print $2}' "$pid/sched")
-  nr_switches=$(awk -F: 'nr_switches {print $2}' "$pid/sched")
+	sum_exec_runtime=$(
+		awk -F: '/se.sum_exec_runtime/ {print $2}' "/proc/$pid/sched"
+	)
 
-  ART=$(bc <<< "scale=3;$sum_exec_runtime/$nr_switches")
-done
+	nr_switches=$(awk -F: '/nr_switches/ {print $2}' "/proc/$pid/sched")
+
+	average_running_time=$(
+		echo "{$sum_exec_runtime/$nr_switches}" | awk '{printf "%.3f", $1}'
+	)
+
+	echo "$pid:$ppid:$average_running_time"
+
+done \
+	| sort -t ':' -k2n \
+	| awk -F ':' '{
+    print "ProcessID="$1" : Parent_ProcessID="$2" : Average_Running_Time="$3
+  }' >2-4-output.txt
