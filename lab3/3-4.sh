e1fea4c3d59ee9d594abd62ab840b2038a3176fb
zzucch <nikitamilovatskiy@gmail.com>
2024-03-22 17:46:24 +0300

    feat: polish 3-4

--- lab3/3-4.sh
+++ lab3/3-4.sh
@@ -3,15 +3,21 @@
 name="3-4-calculations.sh"
 cpu_usage_limit=10
 
-third=$(pgrep -f "$name" | tail -n 1)
-current_niceness=$(ps -o ni -p "$third" | tail -n 1)
+first=$(pgrep -f "$name" | head -n 1)
 
-echo "$third : $current_niceness"
+if [[ -z $first || $first = 0 ]]; then
+	echo "no processes found"
+	exit 1
+fi
+
+current_niceness=$(ps -o ni -p "$first" | tail -n 1)
+
+echo "$first : $current_niceness"
 
 stopped=false
 
 while true; do
-	current_cpu_usage=$(ps -p "$third" -o %cpu --no-headers)
+	current_cpu_usage=$(ps -p "$first" -o %cpu --no-headers)
 	current_cpu_usage=${current_cpu_usage%.*}
 
 	sleeping_time=1
@@ -21,21 +27,21 @@ while true; do
 	if [[ $current_cpu_usage -gt $cpu_usage_limit ]]; then
 		if [[ $current_niceness -eq 20 ]]; then
 			if [[ $stopped = true ]]; then
-				kill -CONT "$third"
+				kill -CONT "$first"
 				stopped=false
 			else
-				kill -STOP "$third"
+				kill -STOP "$first"
 				stopped=true
 
 				sleeping_time=$((current_cpu_usage / 10))
-				echo "stopping for $sleeping_time"
+				echo "stopping for $sleeping_time seconds"
 			fi
 		else
-			renice -n $((current_niceness + 1)) -p "$third"
+			renice -n $((current_niceness + 1)) -p "$first"
 			current_niceness=$((current_niceness + 1))
 		fi
 	elif [[ $current_cpu_usage -lt $cpu_usage_limit ]]; then
-		renice -n $((current_niceness - 1)) -p "$third"
+		renice -n $((current_niceness - 1)) -p "$first"
 		current_niceness=$((current_niceness - 1))
 	fi
 
