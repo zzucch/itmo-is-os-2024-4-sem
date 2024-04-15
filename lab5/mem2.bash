e7d30c4c6ba28125795e62767477c7c4d2f0aacc
zzucch <nikitamilovatskiy@gmail.com>
2024-04-15 22:19:11 +0300

    feat: add mem2

---
+++ lab5/mem2.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+script_dir=$(dirname "$0")
+report_log_filename=$script_dir/report2.log
+offset=100000
+
+true >"$report_log_filename"
+
+array=()
+step=0
+
+while true; do
+	for i in {1..10}; do
+		array+=("$i")
+
+		((step++))
+
+		if ((step % offset == 0)); then
+			echo ${#array[@]} >>"$report_log_filename"
+		fi
+	done
+done
