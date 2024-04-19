dc015faae17af6a9d57fcc788f4ff665bc55cc49
zzucch <nikitamilovatskiy@gmail.com>
2024-04-19 20:54:26 +0300

    fix: resolve regex issue

--- lab4/upback.sh
+++ lab4/upback.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 backup_dir=$(
-	find "$HOME" -maxdepth 1 -type d -name 'Backup-*' -printf '%f\n' |
+	find "$HOME" -maxdepth 1 -type d -name 'Backup-*' -mtime -7 -printf '%f\n' |
 		sort -r |
 		head -1
 )
 
 destination_dir="$HOME/restore"
-regex="^[^.]+\\.[0-9]{4}-[0-9]{2}-[0-9]{2}(\\.[^.]+)?$"
+if [[ ! -d $destination_dir ]]; then
+	mkdir "$destination_dir"
+fi
 
-find "$backup_dir" -type f -regextype posix-extended -regex ".$regex" \
+regex=".*[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}.*"
+
+find "$HOME/$backup_dir" -type f -regextype posix-extended ! -regex "$regex" \
 	-exec cp {} "$destination_dir" \;
