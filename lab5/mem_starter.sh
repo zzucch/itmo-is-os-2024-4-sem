7a98d7d50433060e6aa54862cad85c2a18631d39
zzucch <nikitamilovatskiy@gmail.com>
2024-05-02 22:51:51 +0300

    feat: make monitoring stop on end of mem and mem2

--- lab5/mem_starter.sh
+++ lab5/mem_starter.sh
@@ -3,8 +3,15 @@
 script_dir=$(dirname "$0")
 
 bash "$script_dir"/mem.sh &
+mem_pid=$!
+
 bash "$script_dir"/mem2.sh &
+mem2_pid=$!
 
 bash "$script_dir"/monitor.sh &
+monitor_pid=$!
 
-wait
+wait "$mem_pid" "$mem2_pid"
+
+kill "$monitor_pid"
+wait "$monitor_pid"
