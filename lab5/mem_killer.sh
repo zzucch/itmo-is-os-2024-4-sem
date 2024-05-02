80ad799dcab00519190d148e8e31958e24632932
zzucch <nikitamilovatskiy@gmail.com>
2024-05-02 17:23:49 +0300

    feat: add mem starter and killer

---
+++ lab5/mem_killer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+mem_pid=$(pgrep -f mem.sh)
+if [[ -n $mem_pid ]]; then
+	echo "killing mem.sh with PID $mem_pid.."
+	kill "$mem_pid"
+fi
+
+monitor_pid=$(pgrep -f monitor.sh)
+if [[ -n $monitor_pid ]]; then
+	echo "killing monitor.sh with PID $monitor_pid.."
+	kill "$monitor_pid"
+fi
+
+echo "done"
---
+++ lab5/mem_starter.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+script_dir=$(dirname "$0")
+
+bash "$script_dir"/mem.sh &
+
+bash "$script_dir"/monitor.sh &
+
+wait
