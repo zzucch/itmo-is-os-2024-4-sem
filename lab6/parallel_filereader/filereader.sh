5c0e6d6416f6ccb4d6c06210fc4d6c9e098f0c25
zzucch <nikitamilovatskiy@gmail.com>
2024-05-17 17:47:47 +0300

    feat: add parallel filereader

---
+++ lab6/parallel_filereader/filereader.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+	echo "invalid arguments amount"
+	echo "usage: $(basename "$0") N"
+
+	exit 1
+fi
+
+function filereader() {
+	filename="file_$1.txt"
+
+	while read -r value; do
+		new_value=$((value * 2))
+		echo "$new_value" >>"trash.txt"
+	done <"$filename"
+}
+
+export -f fileworker
+
+seq "$1" | parallel -j0 filereader
