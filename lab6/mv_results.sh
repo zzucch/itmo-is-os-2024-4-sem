2e15f2f573b16380428f4593c336a13b418d141d
zzucch <nikitamilovatskiy@gmail.com>
2024-05-17 20:12:11 +0300

    feat: add more result paths

--- lab6/mv_results.sh
+++ lab6/mv_results.sh
@@ -4,6 +4,11 @@ if [[ ! -d ./results ]]; then
 	mkdir -p ./results
 fi
 
+if [[ -d ./parallel ]]; then
+	cp ./parallel/* ./results
+fi
+
 cp ./factorial/results/* ./results/
+cp ./filereader/results/* ./results/
 cp ./parallel_factorial/results/* ./results/
 cp ./parallel_filereader/results/* ./results/
